ac5876b8d2324e65f420a80291fee453516c5e66
Coder1010 <coderayush1010@gmail.com>
2024-01-27 12:43:54 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+""" 
+  importing all the modules that is needed below here
+
+"""
+
 from pymed import PubMed
 from typing import List
 from haystack import component
@@ -10,14 +15,30 @@ from haystack.components.builders.prompt_builder import PromptBuilder
 import gradio as gr
 import time
 
+
+""" 
+
+  loading env file and getting hugging face api key for credential logging and fetching information
+
+"""
 load_dotenv()
 os.environ['HUGGINGFACE_API_KEY'] = os.getenv('HUGGINGFACE_API_KEY')
 
-
+""" 
+  pubmed is an open source library which contains vector database for medical related data
+  it consists of research paper , medicine and disease data's eigen vector pretrained.
+"""
 pubmed = PubMed(tool="Haystack2.0Prototype", email="coderayush1010@gmail.com")
 
 def documentize(article):
   return Document(content=article.abstract, meta={'title': article.title, 'keywords': article.keywords})
+
+
+""" 
+    this class fetches information from pubmed for any given queries
+    which is further processes by the llm model 
+
+"""
 
 @component
 class PubMedFetcher():
@@ -45,6 +66,15 @@ llm = HuggingFaceTGIGenerator("mistralai/Mixtral-8x7B-Instruct-v0.1")
 llm.warm_up()
 
 
+
+""" 
+
+template for llm in which it willl process the queries and convert it into 
+below format.
+
+"""
+
+
 keyword_prompt_template = """
 Your task is to convert the following question into 3 keywords that can be used to find relevant medical research papers on PubMed.
 Here is an examples:
@@ -58,9 +88,13 @@ question: {{ question }}
 keywords:
 """
 
+"""
+for geeting better result give better promts
+"""
+
 prompt_template = """
 Answer the question truthfully based on the given documents.
-If the documents don't contain an answer, use your existing knowledge base.
+If the documents don't contain an answer, use your existing knowledge base and give comprehensive answer make bullets points if possible.
 
 q: {{ question }}
 Articles:
@@ -72,10 +106,20 @@ Articles:
 
 """
 
+
+"""
+  setting up promptimg templates
+"""
 keyword_prompt_builder = PromptBuilder(template=keyword_prompt_template)
 
 prompt_builder = PromptBuilder(template=prompt_template)
 fetcher = PubMedFetcher()
+
+
+"""
+  building pipeline for the whole Architecture.
+  Architecture is give on github page.
+"""
 
 pipe = Pipeline()
 
@@ -94,7 +138,7 @@ pipe.connect("prompt_builder.prompt", "llm.prompt")
 def ask(question):
   output = pipe.run(data={"keyword_prompt_builder":{"question":question},
                           "prompt_builder":{"question": question},
-                          "llm":{"generation_kwargs": {"max_new_tokens": 600}}})
+                          "llm":{"generation_kwargs": {"max_new_tokens": 800}}})
   print(question)
   print(output['llm']['replies'][0])
   return output['llm']['replies'][0]
@@ -106,6 +150,7 @@ iface = gr.Interface(fn=ask, inputs=gr.Textbox(
         title="LLM Augmented Q&A over PubMed Search Engine",
         description="Ask a question about BioMedical and get an answer from a friendly AI assistant.",
         examples=[["How are mRNA vaccines being used for cancer treatment?"], 
+                ["Give me a case study on Cancer diseas."]
                 ["Suggest me some Case Studies related to Pneumonia."],
                 ["Tell me about HIV AIDS."],["Suggest some case studies related to Auto Immune Disorders."],
                 ["How to treat a COVID infected Patient?"]],
